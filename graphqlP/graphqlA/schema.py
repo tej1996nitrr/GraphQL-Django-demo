995f797e693bd1d9c34dc0a699b39849cb34a00f
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-16 21:18:59 +0530

    mutations

--- graphqlP/graphqlA/schema.py
+++ graphqlP/graphqlA/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from . import models
 from graphene_django.filter.fields  import DjangoFilterConnectionField
+import json
 class MessageType(DjangoObjectType):
     class Meta:
         model=models.Message
@@ -12,3 +13,20 @@ class Query(graphene.AbstractType):
     all_messages = DjangoFilterConnectionField(MessageType)
     def resolve_all_messages(self,context,**kwargs):
         return models.Message.objects.all()
+
+class CreateMessage(graphene.Mutation):
+    class Input:
+        message = graphene.String()
+
+    form_errors = graphene.String()
+    message = graphene.Field(lambda:MessageType)
+    
+    @staticmethod
+    def mutate(root, args, context, info):
+        if not context.user.is_authenticated():
+            return CreateMessage(form_errors=json.dumps('Please login!'))
+        message = models.Message.objects.create(
+            user=context.user, message=args.get('message'))
+        return CreateMessage(message=message, form_errors=None)
+class Mutation(graphene.AbstractType):
+    create_message = CreateMessage.Field()
