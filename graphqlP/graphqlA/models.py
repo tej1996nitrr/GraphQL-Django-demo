d8f994118b5073d0126b36ddeddae0e6897ddd39
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-16 17:10:57 +0530

    created model

--- graphqlP/graphqlA/models.py
+++ graphqlP/graphqlA/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    user = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    creation_date  = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+    
+    
