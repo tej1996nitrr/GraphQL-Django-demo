b4b91ed8e1dd26f975a1c55f19eff1861a8dd1b0
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-16 21:19:25 +0530

    included mutations in project

--- graphqlP/graphqlP/schema.py
+++ graphqlP/graphqlP/schema.py
@@ -1,7 +1,8 @@
 import graphene
 from graphqlA import schema 
 
-
+class Mutation(schema.Mutation,graphene.ObjectType):
+    pass
 class Query(schema.Query,graphene.ObjectType):
     pass    
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query,mutation=Mutation)
